b2c7d47ec3fe6f18546b48ce3a7c683082e6e873
ugabb <gsb.alternativo@gmail.com>
2023-10-04 20:03:05 -0300

    first commit. Basics setups

---
+++ src/components/Logo.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <h1 className='text-white'><span className='text-mainPurple'>Dev</span>Spotlight</h1>
+  )
+}
+
+export default Logo
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <div className='text-white'>
+      <h1>DevSpotlight</h1>
+    </div>
+  )
+}
+
+export default login
