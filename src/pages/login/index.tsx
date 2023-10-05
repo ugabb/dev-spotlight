c5ea899125f11841b733da08f7e9a5ee5358f117
Gabriel Silva Barros <08175460148@mma.gov.br>
2023-10-05 13:56:53 -0300

    auth: trying to create authentication

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
+import { useSession, signIn, signOut } from "next-auth/react"
+
 const login = () => {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        Signed in as {session?.user?.email} <br />
+        <button onClick={() => signOut()}>Sign Out</button>
+      </>
+    )
+  }
+
   return (
-    <div className='text-white'>
+    <div className='text-white bg-mainPurple'>
       <h1>DevSpotlight</h1>
+      <p>Not Signed in</p>
+      <button type="button" onClick={() => signIn()}>Login</button>
     </div>
   )
 }
---
+++ src/pages/projects/details/[id].tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProjectsDetails = () => {
+  return (
+    <div>ProjectsDetails</div>
+  )
+}
+
+export default ProjectsDetails
