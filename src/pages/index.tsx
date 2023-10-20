9acdabe179c6b70f308e25f2cfb397939bb7f654
ugabb <gsb.alternativo@gmail.com>
2023-10-20 06:52:32 -0300

    login authentication

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,37 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { getSession, signIn, signOut, useSession } from 'next-auth/react'
+import { GetServerSideProps } from 'next';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const { data: session } = useSession();
+  console.log(session)
   return (
-    <main>
-      
+    <main className='flex flex-col'>
+      <h1 className='text-4xl font-bold'>Home</h1>
+      <p>{session?.user?.name}</p>
+      <p>{session?.user?.email}</p>
+      <button onClick={() => signOut()} className='bg-mainPurple px-5 py-1 rounded font-bold hover:bg-mainPurple/50'>Sign out</button>
     </main>
   )
+}
+
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context)
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false
+      }
+    }
+  }
+  return {
+    props: {
+      session
+    }
+  }
 }
