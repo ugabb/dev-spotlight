909f5e33bcf5b8b563614953ea786c5525fa8bd6
ugabb <gsb.alternativo@gmail.com>
2023-12-06 16:36:01 -0300

    fix(Button Wide): update width in ButtonWide.tsx

--- src/components/ButtonWide.tsx
+++ src/components/ButtonWide.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const ButtonWide = ({ text, icon }: Props) => {
     return (
-        <div className='flex justify-center items-center gap-3 rounded-xl bg-[#1e1d1f] hover:border-2 hover:border-mainPurple h-[140px] md:w-[250px] xl:w-[400px]'>
+        <div className='flex justify-center items-center gap-3 rounded-xl bg-[#1e1d1f] hover:border-2 hover:border-mainPurple h-[140px] md:w-[250px] xl:w-[350px]'>
             <p className='text-lg text-mainGray hover:text-white '>{text}</p>
             {icon}
         </div>
