c7db7f2b20c5fa7155cd5d66e5285505b578cb50
ugabb <gsb.alternativo@gmail.com>
2023-12-14 18:13:43 -0300

    Add IUser interface and update authentication callbacks to create user when is not in the database

---
+++ src/interfaces/IUser.ts
@@ -0,0 +1,11 @@
+export interface IUser {
+    name: string
+    username: string
+    email: string
+    githubProfileLink: string
+    githubProfilePhoto: string
+    followers: number
+    favoritesRepositories: any[]
+    role: string
+  }
+  
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,5 @@
 import { ProfileContext } from "@/context/ProfileContext"
+import { IUser } from "@/interfaces/IUser"
 import NextAuth, { Awaitable, Session, User } from "next-auth"
 import GithubProvider, { GithubProfile } from "next-auth/providers/github"
 import { useContext } from "react"
@@ -10,25 +11,13 @@ export const authOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      profile(profile) {
-        console.log("profile: ", profile)
-        // console.log(token)
-        return {
-          id: profile.id,
-          name: profile.name,
-          email: profile.email,
-          image: profile.avatar_url,
-          username: profile.login,
-          uid: profile.id
-        }
-      },
     }),
     // ...add more providers here
   ],
   callbacks: {
     async jwt({ token, account, profile }) {
       // Persist the OAuth access_token and or the user id to the token right after signin
-      console.log(profile?.login)
+      console.log(profile)
       if (account) {
         token.accessToken = account.access_token
         token.id = profile.id
@@ -37,13 +26,14 @@ export const authOptions = {
         token.email = profile.email
         token.githubProfileLink = profile.html_url
         token.githubProfilePhoto = profile.avatar_url
+        token.followers = profile.followers
       }
-      console.log("PRORORORR", profile)
+      // console.log("PRORORORR", profile)
       return token
     },
-    async session({ session, token, user }) {
+    async session({ session, token }) {
       // Send properties to the client, like an access_token and user id from a provider.
-      console.log(token)
+      // console.log(token)
       session.accessToken = token.accessToken
       session.user.id = token.id
       session.user.username = token.username
@@ -51,26 +41,52 @@ export const authOptions = {
       session.user.email = token.email
       session.user.githubProfileLink = token.githubProfileLink
       session.user.githubProfilePhoto = token.githubProfilePhoto
+      session.user. followers = token.followers
 
+      const user: IUser = {
+        email: session.user.email,
+        name: session.user.name,
+        username: session.user.username,
+        githubProfileLink: session.user.githubProfileLink,
+        githubProfilePhoto: session.user.githubProfilePhoto,
+        followers: session.user.followers,
+        favoritesRepositories: [],
+        role: "USER"
+      }
+
+      try {
+        const userResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/username/${session.user.username}`)
+        if (!userResponse.ok) {
+          throw new Error('User not found');
+        }
+      } catch (error) {
+        console.log(error)
+        try {
+          // Create a new user in the database
+          const createUserResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+          });
+    
+          if (!createUserResponse.ok) {
+            throw new Error('Failed to create user');
+          }
+    
+          console.log("New user created successfully");
+        } catch (error) {
+          console.error("Failed to create user:", error.message);
+        }
+
+        
+      }
 
       return session
     }
   },
 }
 
+
 export default NextAuth(authOptions)
-
-   
-// @NotBlank
-// private String name;
-// @NotBlank
-// private String username;
-// @Email
-// private String email;
-// @NotBlank
-// private String githubProfileLink;
-// private String githubProfilePhoto;
-// private List<Followers> followers;
-// private List<FavRepositories> favoritesRepositories;
-// private Role role;
-
