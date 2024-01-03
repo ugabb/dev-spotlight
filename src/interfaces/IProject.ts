11fcd826ab0a456ad0d9ed814bb598bcbe6acec1
ugabb <gsb.alternativo@gmail.com>
2024-01-03 14:50:51 -0300

    Update IProject interface in IProject.ts

--- src/interfaces/IProject.ts
+++ src/interfaces/IProject.ts
@@ -1,14 +1,17 @@
 export interface IProject {
-    id: number
-    name: string
-    description: string
-    linkRepo: string
-    technologies: Technology[]
-    likes: number
-    userId: number
+    name: string;
+    linkRepo: string;
+    description: string;
+    deployedLink: string;
+    technologies: ITechnologies[];
+    projectImages?: IProjectImages[];
+    likes: number;
+    userId: number;
 }
-export interface Technology {
-    name: string
-    icon: string
-    repository_id: any
+export interface ITechnologies {
+    name: string;
+    // color: string;
+}
+export interface IProjectImages {
+    url: string;
 }
