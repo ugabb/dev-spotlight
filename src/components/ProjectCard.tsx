3702448b85aadfb6ce7184f256f851cee2e0a360
Gabriel Silva Barros <08175460148@mma.gov.br>
2024-01-04 13:15:57 -0300

    update(Project Card): animation and list of tachnologies tags

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -11,6 +11,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { motion } from 'framer-motion'
 import { IProject } from '@/interfaces/IProject';
+import { useSession } from 'next-auth/react';
 
 type Props = {
     project: IProject
@@ -25,16 +26,25 @@ const ProjectCard = ({ project }: Props) => {
     const handleIconHeartHide = () => {
         setIconHeart(false);
     }
+
+    // const {data:session} = useSession();
+    // const userProfilePhoto = session.user.githubProfilePhoto;
+
     return (
         <motion.div
+            initial={{ opacity: 0, }}
+            animate={{ opacity: 1,  }}
+            transition={{ duration: 1 }}
 
-            className='flex flex-col  bg-white/5 backdrop-blur-sm  border border-gray-500  rounded-[27px]  p-2 relative'>
+            className='flex flex-col  bg-white/5 backdrop-blur-sm  border border-gray-500  rounded-[27px]  p-2 relative max-w-[400px]'>
             <div className='flex gap-3 justify-between items-center '>
                 <div className='flex flex-col w-10 h-10'>
                     <Image className='rounded-full object-cover' src={"/user.jpg"} width={40} height={40} alt='profile picture' />
                     <p className='text-mainGray text-xs flex items-center'>ugabb</p>
                 </div>
-                <h2 className='text-xl font-bold break-all'>{project?.name}</h2>
+
+                <h2 className='text-xl font-bold truncate hover:text-mainPurple'>{project?.name}</h2>
+
                 <motion.div
                     onMouseEnter={handleIconHeartShow} onMouseLeave={handleIconHeartHide} className='transition-all ease-in-out cursor-pointer'>
                     {iconHeart ? <GoHeartFill size={25} className='text-mainPurple' /> : <GoHeart size={25} />}
@@ -42,9 +52,9 @@ const ProjectCard = ({ project }: Props) => {
             </div >
 
             <div className="flex flex-col w-full mt-5 px-3">
-                <div className='flex justify-center  w-full h-32'>
-                    <Image className='object-cover rounded-md w-full ' src={"/man-computer.jpg"} width={1920} height={1280} alt='profile picture' />
-                </div>
+                <Link href={`/projects/details/${project.name}`} className='flex justify-center  w-full h-32 hover:border hover:border-mainPurple hover:rounded-md'>
+                    <Image className='object-cover rounded-md w-full ' src={project.projectImages[0].url} width={1920} height={1280} alt='profile picture' />
+                </Link>
             </div>
 
             <div className='p-3 text-mainGray w-full'>
@@ -52,9 +62,9 @@ const ProjectCard = ({ project }: Props) => {
                 <p className='text-sm leading-4 break-words'>{project?.description}</p>
             </div>
 
-            <div className="flex gap-3 text-sm px-3">
-                {project?.technologies.map((tech) => (
-                    <TextIcon key={tech.name} text={tech.name} textColor='mainGray' icon={<FaReact className='text-blue-500' />} wrap='col' />
+            <div className="flex flex-wrap gap-1 text-sm px-3 my-3">
+                {project?.technologies.map((tech,id) => (
+                    <span className={`text-mainPurple text-xs p-1 rounded-md border border-mainPurple`} key={id}>{tech.name}</span>
                 ))}
             </div>
 
