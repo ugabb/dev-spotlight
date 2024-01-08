5066ac43a6568cae25c88d2126dfc198b472f79e
Gabriel Silva Barros <08175460148@mma.gov.br>
2024-01-08 12:57:18 -0300

    feat: create footer

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Logo = () => {
   return (
-    <h1 className='text-white inline-block'><span className='text-mainPurple'>Dev</span>Spotlight</h1>
+    <h1 className='text-white inline-block'><span className='text-mainPurple'>Dev</span><span className='glow-text'>Spotlight</span></h1>
   )
 }
 
