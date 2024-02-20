cef3a2f156cc58a69f195c914d24ed6c052ab2d1
Gabriel Silva Barros <08175460148@mma.gov.br>
2024-02-20 17:13:29 -0300

    fix: delete the old user creation and return the session

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -68,48 +68,38 @@ export const authOptions: AuthOptions = {
       return true;
     },
 
-    // async jwt({ token, account, profile }) {
-    //   // Persist the OAuth access_token and or the user id to the token right after signin
-    //   if (account) {
-    //     token.accessToken = account.access_token;
-    //     token.id = profile.id;
-    //     token.username = profile.login;
-    //     token.name = profile.name;
-    //     token.email = profile.email;
-    //     token.githubProfileLink = profile.html_url;
-    //     token.githubProfilePhoto = profile.avatar_url;
-    //     token.followers = profile.followers;
-    //   }
-    //   // console.log("PRORORORR", profile)
-    //   return token;
-    // },
-    // async session({ session, token }) {
-    //   // Send properties to the client, like an access_token and user id from a provider.
-    //   // console.log(token)
-    //   session.accessToken = token.accessToken;
-    //   session.user.id = token.id;
-    //   session.user.username = token.username;
-    //   session.user.name = token.name;
-    //   session.user.email = token.email;
-    //   session.user.githubProfileLink = token.githubProfileLink;
-    //   session.user.githubProfilePhoto = token.githubProfilePhoto;
-    //   session.user.followers = token.followers;
+    async jwt({ token, account, profile }) {
+      // Persist the OAuth access_token and or the user id to the token right after signin
+      console.log(token,account,profile)
+      if (account) {
+        token.accessToken = account.access_token;
+        token.id = profile.id;
+        token.username = profile.login;
+        token.name = profile.name;
+        token.email = profile.email;
+        token.githubProfileLink = profile.html_url;
+        token.githubProfilePhoto = profile.avatar_url;
+        token.followers = profile.followers;
+      }
+      // console.log("PRORORORR", profile)
+      return token;
+    },
+    async session({ session, token }) {
+      // Send properties to the client, like an access_token and user id from a provider.
+      // console.log(token)
+      session.accessToken = token.accessToken;
+      session.user.id = token.id;
+      session.user.username = token.username;
+      session.user.name = token.name;
+      session.user.email = token.email;
+      session.user.githubProfileLink = token.githubProfileLink;
+      session.user.githubProfilePhoto = token.githubProfilePhoto;
+      session.user.followers = token.followers;
 
-    //   const user: IUser = {
-    //     email: session.user.email,
-    //     name: session.user.name,
-    //     username: session.user.username,
-    //     githubProfileLink: session.user.githubProfileLink,
-    //     githubProfilePhoto: session.user.githubProfilePhoto,
-    //     followers: session.user.followers,
-    //     favoritesRepositories: [],
-    //     role: "USER",
-    //   };
+      console.log(session);
 
-    //   console.log({ user });
-
-    //   return session;
-    // },
+      return session;
+    },
   },
 };
 
