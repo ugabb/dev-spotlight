22e642fb8a2f443992a175099009fb44ed69d1b7
Gabriel Silva Barros <08175460148@mma.gov.br>
2024-02-20 17:12:01 -0300

    feat: add react-toast-hot configuration

---
+++ src/context/ToasterProvider.tsx
@@ -0,0 +1,8 @@
+import { Toaster } from "react-hot-toast"
+
+const ToasterContext = () => {
+    return (
+        <Toaster position="top-center" reverseOrder={false} />
+    )
+}
+export default ToasterContext
