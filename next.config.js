95462f8581a7d65b836ad54e8fd9869249cef1d5
Gabriel Silva Barros <08175460148@mma.gov.br>
2024-02-27 16:47:38 -0300

    update: domain now allows source from cloudinary

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images:{domains:["avatars.githubusercontent.com","firebasestorage.googleapis.com","m.economictimes.com","www.flipar.com.br",'i0.wp.com']}
+  images: { domains: ["avatars.githubusercontent.com", "firebasestorage.googleapis.com", "m.economictimes.com", "www.flipar.com.br", 'i0.wp.com', "res.cloudinary.com"] }
 }
 
 module.exports = nextConfig
