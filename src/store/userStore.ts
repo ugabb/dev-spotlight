41abd550b9f8c364f7294413521ea63c76ab7808
Gabriel Silva Barros <08175460148@mma.gov.br>
2024-03-07 13:54:47 -0300

    feat: zustand global state for the currentUser

---
+++ src/store/userStore.ts
@@ -0,0 +1,31 @@
+import { Project, ProjectsLiked, User } from "@prisma/client";
+import axios from "axios";
+import { create } from "zustand";
+
+interface IUserStore {
+  currentUser: User;
+  projects: Project[];
+  projectsLiked: ProjectsLiked[];
+}
+
+interface ICurrentUserStore {
+  currentUser: IUserStore | undefined;
+  setCurrentUser: (username: string) => void;
+}
+
+const userStore = create<ICurrentUserStore>()((set) => ({
+  currentUser: undefined,
+  setCurrentUser: async (username: string) => {
+    try {
+      const { data } = await axios.get(`/api/users/username/${username}`);
+      console.log(data);
+      set({
+        currentUser: data as IUserStore,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+}));
+
+export default userStore;
