ee1e742687f0b9cfbc038c38da6b9ff6ad27c082
Gabriel Silva Barros <08175460148@mma.gov.br>
2024-03-19 18:05:21 -0300

    fix: fixing components rendering twice

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: { domains: ["avatars.githubusercontent.com","www.notion.so", "firebasestorage.googleapis.com", "m.economictimes.com", "www.flipar.com.br", 'i0.wp.com', "res.cloudinary.com"] }
 }
 
