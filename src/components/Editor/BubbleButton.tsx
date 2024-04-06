4a26ef1b86b07a674e1c56a47b82109bacac87f1
Gabriel Barros <ugabrieldev@gmai.com>
2024-04-06 11:48:57 -0300

    Update BubbleButton className to include hover:bg-mainPurple/50

--- src/components/Editor/BubbleButton.tsx
+++ src/components/Editor/BubbleButton.tsx
@@ -4,7 +4,7 @@ export interface BubbleButtonProps extends ComponentProps<'button'>{}
 
 const BubbleButton = ({...props}: BubbleButtonProps) => {
     return (
-        <button className='p-2 text-sm text-zinc-200 hover:text-zinc-50 rounded-lg  bg-transparent hover:bg-mainPurple data-[active=true]:bg-mainPurple' {...props}/>
+        <button className='p-2 text-sm text-zinc-200 hover:text-zinc-50 rounded-lg  bg-transparent hover:bg-mainPurple/50 data-[active=true]:bg-mainPurple' {...props}/>
     )
 }
 
